fa9fed6593f89502e75fecfd77a46d680a8a76e2
Adem KOCAMAZ <kocamazadem@gmail.com>
2024-05-21 19:09:25 +0300

    Initial Commit
    -Project created.
    -Dependencies added.

---
+++ app/build.gradle.kts
@@ -0,0 +1,115 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "ilkadam.ilkmuhabbet"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ilkadam.ilkmuhabbet"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Foundation
+    implementation("androidx.compose.foundation:foundation:1.6.7")
+
+    //OneSignal
+    implementation("com.onesignal:OneSignal:[5.0.0, 5.99.99]")
+
+    //Icons
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+
+    //Dagger-Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.11.0")
+
+    //Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-storage")
+
+    //DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore:1.1.1")
+
+    //NavigationWithCompose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //KotlinCoroutinePlayServices
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
+}
---
+++ app/src/main/java/ilkadam/ilkmuhabbet/ilkApplication.kt
@@ -0,0 +1,8 @@
+package ilkadam.ilkmuhabbet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ilkApplication:Application() {
+}
