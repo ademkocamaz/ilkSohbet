dad4fa54ad9308eda3167b90bff0db3f144e3bf4
Adem KOCAMAZ <kocamazadem@gmail.com>
2024-05-21 20:16:09 +0300

    Core Package Implemented

---
+++ app/src/main/java/ilkadam/ilkmuhabbet/core/Constants.kt
@@ -0,0 +1,7 @@
+package ilkadam.ilkmuhabbet.core
+
+object Constants {
+    const val ERROR_MESSAGE = "Unexpected error."
+    const val NO_CHATROOM_IN_FIREBASE_DATABASE = "NO_CHATROOM_IN_FIREBASE_DATABASE"
+    const val ONESIGNAL_APP_ID = "d331f52a-0e91-4b0f-af21-7f893955ecc7"
+}
